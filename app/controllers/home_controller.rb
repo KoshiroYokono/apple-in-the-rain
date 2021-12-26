1fa15964bf7e273ccaa068305a421660c336baab
KoshiroYokono <kastsuo.ksr116@gmail.com>
2021-12-26 22:30:49 +0900

    曇りを作り出した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,18 @@
 class HomeController < ApplicationController
   def new
     weather = Weather.last
+    @is_cloudy = false
+    @is_raining = false
     unless weather.nil?
       weather_id = weather.weather_id
-      if weather_id == 800
+      @is_cloudy = false
+      if weather_id < 800
+        @is_raining = true
+      elsif weather_id == 800
         @is_raining = false
       else
-        @is_raining = true
+        @is_cloudy = true
       end
-    else
-      @is_rainning = false
     end
   end
 end
