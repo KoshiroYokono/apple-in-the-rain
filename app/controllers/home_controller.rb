f9bc83f006faa086b6664d8daea72ece70867e28
KoshiroYokono <kastsuo.ksr116@gmail.com>
2021-12-26 17:08:04 +0900

    first-> last

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   def new
-    weather = Weather.first
+    weather = Weather.last
     unless weather.nil?
-      
       weather_id = weather.weather_id
       if weather_id == 800
         @is_rainning = false
