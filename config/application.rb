0de5d6112644b94caae9337d4525d28220476b88
KoshiroYokono <kastsuo.ksr116@gmail.com>
2022-02-27 00:47:41 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Portfolio
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.paths.add 'lib', eager_load: true
+    config.time_zone = 'Tokyo'
   end
 end
