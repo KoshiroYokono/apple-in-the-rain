2c54ca1fd3bc1672dd460082126f5647a99ee58e
KoshiroYokono <kastsuo.ksr116@gmail.com>
2022-02-26 22:54:05 +0900

    ゆきが降るようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,19 @@
 class HomeController < ApplicationController
   def new
     weather = Weather.last
-    @is_cloudy = false
     @is_raining = false
+    @is_snowing = false
     unless weather.nil?
       weather_id = weather.weather_id
-      @is_cloudy = false
-      if weather_id < 800
+      if 600 <= weather_id && weather_id <= 622
+        @is_snowing = true
+        @is_cloudy = true
+      elsif weather_id < 800
         @is_raining = true
+        @is_cloudy = true
       elsif weather_id == 800
         @is_raining = false
+        @is_cloudy = false
       else
         @is_cloudy = true
       end
