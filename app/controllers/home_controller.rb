f6c9ad3226b4190a65d6827171889ab7cbae7bba
KoshiroYokono <kastsuo.ksr116@gmail.com>
2022-02-27 00:48:14 +0900

    4月だけ桜が舞うようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @is_snowing = false
     unless weather.nil?
       weather_id = weather.weather_id
+      @is_cherry_bllosoming = spring_now?
       if 600 <= weather_id && weather_id <= 622
         @is_snowing = true
         @is_cloudy = true
@@ -18,5 +19,11 @@ class HomeController < ApplicationController
         @is_cloudy = true
       end
     end
+  end
+
+  private
+
+  def spring_now?
+    Time.zone.now.month == 4
   end
 end
